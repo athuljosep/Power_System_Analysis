e94b9553e3a2882299bef289ac6c4594ad1a7611
athuljosep <70621782+athuljosep@users.noreply.github.com>
2023-12-08 19:44:54 -0800

    Finally completed prog 3 and submitted on canvas

--- Program_3/J_full_calc.m
+++ Program_3/J_full_calc.m
@@ -1,12 +1,11 @@
-%% Calculation Jacobian Matrix
-function J = J_full_calc(V, delta, P, Q,n_bus,Y)
+function J = J_full_calc(V, T, P, Q,n_bus,Y)
 for m = 1:n_bus 
     for n = 1:n_bus
         if m ~= n
-            J11(m, n) = -abs(V(m) * V(n) * Y(m, n)) * sin(angle(Y(m, n)) + delta(n) - delta(m));
-            J21(m, n) = -abs(V(m) * V(n) * Y(m, n)) * cos(angle(Y(m, n)) + delta(n) - delta(m));
-            J12(m, n) = abs(V(m) * V(n) * Y(m, n)) * cos(angle(Y(m, n)) + delta(n) - delta(m));
-            J22(m, n) = -abs(V(m) * V(n) * Y(m, n)) * sin(angle(Y(m, n)) + delta(n) - delta(m));
+            J11(m, n) = -abs(V(m) * V(n) * Y(m, n)) * sin(angle(Y(m, n)) + T(n) - T(m));
+            J21(m, n) = -abs(V(m) * V(n) * Y(m, n)) * cos(angle(Y(m, n)) + T(n) - T(m));
+            J12(m, n) = abs(V(m) * V(n) * Y(m, n)) * cos(angle(Y(m, n)) + T(n) - T(m));
+            J22(m, n) = -abs(V(m) * V(n) * Y(m, n)) * sin(angle(Y(m, n)) + T(n) - T(m));
         else
             J11(m, m) = -Q(m) - (abs(V(m))^2) * imag(Y(m,m));
             J21(m, m) = P(m) - (abs(V(m))^2) * real(Y(m,m));
--- Program_3/PQline_calc.m
+++ Program_3/PQline_calc.m
@@ -6,13 +6,8 @@ for i = 1:n_br
     Pij(i,1) = -(V(a)^2)*real(Y(a,b)) + abs(V(a)*V(b)*Y(a,b))*cos(angle(Y(a,b)) + T(b) - T(a));
     Pji(i,1) = -(V(b)^2)*real(Y(b,a)) + abs(V(b)*V(a)*Y(b,a))*cos(angle(Y(b,a)) + T(a) - T(b));
    
-%     Qij(i,1) = -((V(a)^2)*(abs(B(b))/2 - imag(Y(a,b))) + abs(V(a)*V(b)*Y(a,b))*sin(angle(Y(a,b)) + T(a) - T(b)));
-%     Qji(i,1) = -((V(b)^2)*(abs(B(a))/2 - imag(Y(b,a))) + abs(V(b)*V(a)*Y(b,a))*sin(angle(Y(b,a)) + T(b) - T(a)));
+    Qij(i,1) = -((V(a)^2)*(abs(B(b))/2 - imag(Y(a,b))) + abs(V(a)*V(b)*Y(a,b))*sin(angle(Y(a,b)) + T(a) - T(b)));
+    Qji(i,1) = -((V(b)^2)*(abs(B(a))/2 - imag(Y(b,a))) + abs(V(b)*V(a)*Y(b,a))*sin(angle(Y(b,a)) + T(b) - T(a)));
     
-
-    Qij(i,1) = - V(a)*V(b)*abs(Y(a,b))*sin(angle(Y(a,b)) + T(b) - T(a)) + V(a).^2*abs(Y(a,b))*sin(angle(Y(a,b)));
-    Qji(i,1) = - V(b)*V(a)*abs(Y(b,a))*sin(angle(Y(b,a)) + T(a) - T(b)) + V(b).^2*abs(Y(b,a))*sin(angle(Y(b,a)));
-    
-  
 end
 end
